a0bf3f44b215f82abff5d4d25f9d76ec6a0a59b7
ashleygagregory <56142786+ashleygagregory@users.noreply.github.com>
2021-01-19 13:49:46 +1100

    Update DeployECTToSitesDoclibs.ps1
    Adjusting exception handling for 401/403 connection errors

--- DeployECTToSitesDoclibs.ps1
+++ DeployECTToSitesDoclibs.ps1
@@ -402,10 +402,10 @@ Try {
         }
 
         Catch [System.Net.WebException] {
-            If ($($_.Exception.Message) -like "*(401) Unauthorized*") {
+            If ($($_.Exception.Message) -like "*(401)*") {
                 Write-Log -Level Warn "Cannot authenticate with SharePoint Admin Site. Please check if an authentication prompt appeared on your machine prior to the last interaction with this script."
             }
-            ElseIf ($($_.Exception.Message) -like "*(403) Forbidden*") {
+            ElseIf ($($_.Exception.Message) -like "*(403)*") {
                 Write-Log -Level Warn "Cannot login to SharePoint Admin Site, Access Denied. Please check the permissions of the credentials/account you are using to authenticate with."
             }
             Throw
